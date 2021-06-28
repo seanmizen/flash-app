c81d954b54d20d7528725f8510c7c6e6ddf95a48
smizzle-probably <sean@seanmizen.com>
2021-06-28 11:30:14 +0100

    Folder structure changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Home from "./Home";
-import Nav from "./Nav";
-import DeckEditor from "./DeckEditor";
-import About from "./About";
+import Home from "./features/Home/Home";
+import Nav from "./core/Nav";
+import DeckEditor from "./features/DeckEditor.txt/DeckEditor";
+import About from "./features/About/About";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 //import './App.css';
 
--- src/core/Nav.js
+++ src/core/Nav.js
@@ -24,5 +24,4 @@ class Nav extends Component {
     }
 };
 
-
 export default Nav;
