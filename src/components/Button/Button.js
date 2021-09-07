29684b9316bc7a0e5f0588ba3847a38a9892c328
Sean <sean@seanmizen.com>
2021-09-07 17:44:27 +0100

    Added wireframes for better projectwide components

---
+++ src/components/Button/Button.js
@@ -0,0 +1,18 @@
+import styles from "./Button.css";
+
+const Button = ({ children, className, ...others }) => {
+  return (
+    <button className={styles["button-one"] + " " + className} {...others}>
+      {children}
+    </button>
+  );
+};
+
+//Simple default button:
+/*
+const Button = ({ children, ...others }) => {
+  return <button {...others}></button>;
+};
+*/
+
+export default Button;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as ItemList } from "./ItemList/ItemList";
+
+//This file exists so we can then do a much simpler import later:
+//import {ItemList} from "../components"
