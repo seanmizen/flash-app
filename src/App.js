56953485f655fb7a70d47405528d662331ae9b96
Sean <sean@seanmizen.com>
2021-11-17 18:42:09 +0000

    Added /flash-app to router base path

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/flash-app">
       <Nav />
       <div className="container">
         <Switch>
